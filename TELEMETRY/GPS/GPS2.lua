5ddf9a118118c9fb29fa9af5ea5afeedb3d399d1
RCdiy <info@RCdiy.ca>
2017-12-23 16:45:09 -0500

    Added [16] = "N"
    When converting 349 and above to a compass rose direction the index
    returned was 16 (North) which was not accounted for.

--- TELEMETRY/GPS/GPS2.lua
+++ TELEMETRY/GPS/GPS2.lua
@@ -325,7 +325,8 @@ local DebuggingSkip = 10
 local CompassRose16Table = {  [0] = "N", [1] = "NNE", [2] = "NE", [3] = "ENE",
                               [4] = "E", [5] = "ESE" , [6] = "SE", [7] = "SSE",
                               [8] = "S", [9] = "SSW", [10] = "SW", [11] = "WSW",
-                              [12] = "W", [13] = "WNW", [14] = "NW", [15] = "NNW"
+                              [12] = "W", [13] = "WNW", [14] = "NW", [15] = "NNW",
+                              [16] = "N"
                             }
 
 -- *** This is used to access the Turn sound files
